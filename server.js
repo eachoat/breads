67b3ce99c507a8397d605869f885a77ba17705da
eachoat <eachoat@gmail.com>
2024-01-21 14:32:01 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require ('express')
+
+//Configuration
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Welsome to an Awesome App about Breads!')
+})
+
+//Breads
+const breadsController = require ('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+//Listen
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
+
+console.log(PORT)
